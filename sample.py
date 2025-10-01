45bc6bf6bc147cacaa7737bee20915a716d8c58e
laoshanxi <178029200@qq.com>
2025-10-01 20:17:28 +0800

    sample

--- sample.py
+++ sample.py
@@ -1,52 +1,50 @@
-"""Example for App Mesh Client."""
-
 #!/usr/bin/python3
-# Ensure you have the App Mesh client library installed:
-# python3 -m pip install --upgrade appmesh
+# Example for App Mesh Client.
+# Install appmesh package: python3 -m pip install --upgrade appmesh
 
 from time import sleep
 from appmesh import AppMeshClient, App
 
+client = AppMeshClient()
+client.login("admin", "admin123")
 
-def main() -> None:
-    """Demonstrate basic usage of the App Mesh Client SDK."""
 
-    # Initialize the App Mesh Client
-    client = AppMeshClient()
+def demo_task_execute():
+    """Run a simple remote task 10 times, using the previous result as input."""
+    count_in_server = "0"
+    for i in range(10):
+        task_data = f"print({count_in_server} + {i}, end='')"
+        count_in_server = client.run_task(app_name="pytask", data=task_data)
+        print(count_in_server)
 
-    # Authenticate with App Mesh using username and password
-    if client.login("admin", "admin123"):
 
-        # Define a new application with name, status, and command to execute
-        myapp = App()
-        myapp.name = "myapp"
-        myapp.status = 0  # Indicates the app is initially disabled
-        myapp.command = "ping github.com"
+def demo_app_mgmt():
+    """Show basic app management: add, view, enable, get output, and delete."""
+    myapp = App()
+    myapp.name = "myapp"
+    myapp.status = 0  # 0 = disabled
+    myapp.command = "ping github.com"
 
-        # Add the application to App Mesh
-        response = client.add_app(myapp)
-        print("Application added:", response)
+    # Add app
+    print("Application added:", client.add_app(myapp))
 
-        # View details of the added application
-        app_details = client.view_app(myapp.name)
-        print("Application details:", app_details)
+    # View app details
+    print("Application details:", client.view_app(myapp.name))
 
-        # Enable the application to start running
-        client.enable_app(myapp.name)
-        sleep(3)  # Wait briefly to allow the app to start
+    # Enable and wait
+    client.enable_app(myapp.name)
+    sleep(3)
 
-        # Retrieve and print the application output with a 3-second timeout
-        output_response = client.get_app_output(myapp.name, timeout=3)
-        print("Application output:\n", output_response.output)
+    # Get and print output
+    output = client.get_app_output(myapp.name, timeout=3)
+    print("Application output:\n", output.output)
 
-        # Delete the application after viewing the output
-        delete_response = client.delete_app(myapp.name)
-        print("Application deleted:", delete_response)
-        
-
-    else:
-        print("Login failed. Check credentials.")
+    # Clean up
+    print("Application deleted:", client.delete_app(myapp.name))
 
 
 if __name__ == "__main__":
-    main()
+    print("Start sample...")
+    demo_app_mgmt()
+    demo_task_execute()
+    print("Completed sample")
